8180f9d5fc7866e52b82831b483ece60f0c5d49b
robbe.delsoir <robbe.delsoir@student.howest.be>
2021-03-01 09:46:20 +0100

    Add project files.

---
+++ gitHubTest3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitHubTest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
