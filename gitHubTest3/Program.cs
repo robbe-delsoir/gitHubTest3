904b3c01bc508b842aa107ccae7957062586ea1c
robbe.delsoir <robbe.delsoir@student.howest.be>
2021-03-01 09:49:39 +0100

    Hello World

--- gitHubTest3/Program.cs
+++ gitHubTest3/Program.cs
@@ -7,6 +7,8 @@ namespace gitHubTest3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
+
         }
     }
 }
